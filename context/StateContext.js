45136f466dc9d80dd47d7c7eae0066f28f21f7dc
Cleiton Daniel <87211930+Daniels7k@users.noreply.github.com>
2022-09-21 08:47:31 -0300

    implementing cart logic and layout

--- context/StateContext.js
+++ context/StateContext.js
@@ -6,9 +6,12 @@ const Context = createContext();
 export const StateContext = ({ children }) => {
   const [showCart, setShowCart] = useState(false);
   const [cartItems, setCartItems] = useState([]);
-  const [totalPrice, setTotalPrice] = useState();
-  const [totalQuantities, setTotalQuantities] = useState();
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalQuantities, setTotalQuantities] = useState(0);
   const [qty, setQty] = useState(1);
+
+  let foundProduct;
+  let index;
 
   const onAdd = (product, quantity) => {
     const checkProductInCart = cartItems.find(
@@ -37,6 +40,35 @@ export const StateContext = ({ children }) => {
 
     toast.success(`${qty} ${product.name} adicionado ao carrinho`);
   };
+  
+  const onRemove = (product) => {
+    foundProduct = cartItems.find((item) => item._id === product._id);
+    const newCartItems = cartItems.filter((item) => item._id !== product._id)
+
+    setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price * foundProduct.quantity)
+    setTotalQuantities((prevTotalQuantitys) => prevTotalQuantitys - foundProduct.quantity)
+    setCartItems(newCartItems)
+  }
+
+  // TO-DO: Resolver bug da troca de itens no carrinho
+  const toggleCartItemQuantity = (id, value) => {
+    foundProduct = cartItems.find((item) => item._id === id);
+    index = cartItems.findIndex((product) => product._id === id);
+
+    const newCartItems = cartItems.filter((item) => item._id !== id)
+
+    if (value === "inc") {
+      setCartItems([...newCartItems, {...foundProduct, quantity: foundProduct.quantity + 1}])
+      setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price)
+      setTotalQuantities(prevTotalQuantitys => prevTotalQuantitys + 1)
+    } else if (value === "dec") {
+      if (foundProduct.quantity > 1) {
+      setCartItems([...newCartItems, {...foundProduct, quantity: foundProduct.quantity - 1}])
+      setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price)
+      setTotalQuantities(prevTotalQuantitys => prevTotalQuantitys - 1)
+      }
+    }
+  };
 
   const incQty = () => {
     setQty((prevQty) => prevQty + 1);
@@ -54,6 +86,7 @@ export const StateContext = ({ children }) => {
     <Context.Provider
       value={{
         showCart,
+        setShowCart,
         cartItems,
         totalPrice,
         totalQuantities,
@@ -61,6 +94,8 @@ export const StateContext = ({ children }) => {
         incQty,
         decQty,
         onAdd,
+        toggleCartItemQuantity,
+        onRemove
       }}
     >
       {children}
